0b9e8e49f3d756a6483849ae470ab3e2131f6d0b
Przemysław Owsianik <Przemek.Owsianik@yahoo.com>
2018-10-15 19:12:47 +0200

    Install Body Parser and Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 const portForLocalhost = 4000;
+
+mongoose.connect('mongodb://localhost/blog_app');
 
 app.get('/', (req, res) => {
     res.send("Main page");
